75baf142e0fbfb7c51cbda0718f64ccbad4a3363
Wim Ulkeman <wimulkeman@hotmail.com>
2022-12-23 16:38:16 +0100

    fix: make the string at least one
    Make the string at least one character so the upper arrow /\ can be created for under the stream

--- src/Exception/Parser/InvalidGrammerSequence.php
+++ src/Exception/Parser/InvalidGrammerSequence.php
@@ -16,10 +16,17 @@ class InvalidGrammerSequence extends JsonParserException
 
     private function createErrorMessage(AbstractToken $currentToken, AbstractToken $previousToken): string
     {
+        $previousLexeme = $previousToken->getLexeme();
+        if (strlen($previousLexeme) === 0) {
+            $previousLexeme = ' ';
+        }
+
+        $currentLexeme = $currentToken->getLexeme();
+
         $error = sprintf("Invalid character found at position %s.\n", $currentToken->getPointerStart());
         $error .= sprintf("The allowed characters are [%s]\n", implode(', ', $previousToken->supportedNextTokens()));
-        $error .= sprintf("The error occurred on the following sequence:\n%s%s\n", $previousToken->getLexeme(), $currentToken->getLexeme());
-        $error .= sprintf('%s/\\', str_repeat(' ', strlen($previousToken->getLexeme()) - 1));
+        $error .= sprintf("The error occurred on the following sequence:\n%s%s\n", $previousLexeme, $currentLexeme);
+        $error .= sprintf('%s/\\', str_repeat(' ', strlen($previousLexeme) - 1));
 
         return $error;
     }
